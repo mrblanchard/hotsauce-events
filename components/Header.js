8fba6e763c772abcbb556517cab965e1b054a011
Jeremy Blanchard <mrjeremyblanchard@gmail.com>
2021-07-22 11:30:22 -0400

    Layout

---
+++ components/Header.js
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import styles from '../styles/Header.module.css';
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <Link href='/'><a>Hot Sauce Events</a></Link>
+      </div>
+      <nav>
+          <ul>
+            <li>
+              <Link href='/events'><a>Events</a></Link>
+            </li>
+          </ul>
+        </nav>
+    </header>
+  )
+}
