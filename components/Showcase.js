a42345cbf5eb571a14af3e6533f64e155065d360
Jeremy Blanchard <mrjeremyblanchard@gmail.com>
2021-07-23 13:05:23 -0400

    adding images and adding showcase

---
+++ components/Showcase.js
@@ -0,0 +1,12 @@
+import styles from '@/styles/Showcase.module.css';
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome to the Hot Sauce Events website</h1>
+      <h2>
+        Where you can find the best sauce events in the world.
+      </h2>
+    </div>
+  );
+}
--- components/layout.js
+++ components/layout.js
@@ -1,8 +1,12 @@
-import Head from 'next/head'
-import Header from './Header'
-import Footer from './Footer'
-import styles from '@/styles/Layout.module.css'
-export default function Layout({title, keywords, description, children}){
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import Header from './Header';
+import Footer from './Footer';
+import Showcase from './Showcase';
+import styles from '@/styles/Layout.module.css';
+
+export default function Layout({ title, keywords, description, children }) {
+  const router = useRouter();
   return (
     <div>
       <Head>
@@ -10,17 +14,19 @@ export default function Layout({title, keywords, description, children}){
         <meta name='description' content={description} />
         <meta name='keywords' content={keywords} />
       </Head>
-    <Header />
+      <Header />
+      {router.pathname === '/' && <Showcase />}
       <div className={styles.container}>
-     {children}
-     </div>
-     <Footer />
+        {children}
+      </div>
+      <Footer />
     </div>
-    
-  )}
+
+  );
+}
 
 Layout.defaultProps = {
   title: 'Hot sauce events | Find any hot sauce event',
   description: 'Find the most prominent hot sauces available at an event',
   keywords: 'hot, sauce'
-}
+};
